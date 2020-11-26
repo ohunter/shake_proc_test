f87703d04892b58824b1d9cd4b49b2a40309ee36
Ohunter <ohunter@users.noreply.github.com>
2020-11-26 15:12:48 +0100

    Updated output

--- visualize.py
+++ visualize.py
@@ -10,7 +10,12 @@ if __name__ == "__main__":
 
     mc = sorted([(k, v) for k, v in data["word_counts"].items()], key=lambda item: item[1])[-10:]
 
+    print(f"The 10 most common words in {data['file']}:")
+    for k, v in mc[::-1]:
+        print(f"{k}: {v}")
+
     with open('out.csv', 'w') as fi:
+        fi.write(f"# File: {data['file']}\n")
         fi.write(f"Word,Count\n")
         for k, v in mc[::-1]:
             fi.write(f"{k},{v}\n")
